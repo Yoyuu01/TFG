14f9e945033c19187b492aecfd044e9b091a73ab
yoyuu01 <josuegabriel.barrueta.gutierrez@cieep.com>
2025-05-01 14:46:16 +0200

    interface

---
+++ backend/src/viajes/interfaces/viajes.interface.ts
@@ -0,0 +1,50 @@
+export interface vuelosDto {
+    _id: string;
+    origen: string;
+    destino: string;
+    fecha_salida: string;
+    hora_salida: string;
+    hora_llegada: string;
+    precio: number;
+    aerolinea: string;
+    plazas_disponibles: number;
+}
+
+export interface usuariosDto {
+    _id: string;
+    origen: string;
+    destino: string;
+    fecha_salida: string;
+    hora_salida: string;
+    hora_llegada: string;
+    precio: number;
+    aerolinea: string;
+    plazas_disponibles: number;
+}
+
+export interface reservasDto {
+    _id: string;
+    usuario_id: string;
+    vuelo_id: string;
+    fecha_reserva: string;
+    estado: string;
+    asiento: string;
+}
+
+export interface pagosDto {
+    _id: string;
+    reserva_id: string;
+    monto: number;
+    metodo_pago: string;
+    estado_pago: string;
+    fecha_pago: string;
+}
+
+export interface opinionesDto {
+    _id: string;
+    usuario_id: string;
+    vuelo_id: string;
+    comentario: string;
+    puntuacion: number;
+    fecha: string;
+}
