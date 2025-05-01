d1937d585222711ea483078c7c964190e8ef8a3f
yoyuu01 <josuegabriel.barrueta.gutierrez@cieep.com>
2025-05-01 14:43:09 +0200

    dto

---
+++ backend/src/viajes/dto/viajes.dto.ts
@@ -0,0 +1,51 @@
+export class vuelosDto {
+    _id: string;
+    origen: string;
+    destino: string;
+    fecha_salida: string;
+    hora_salida: string;
+    hora_llegada: string;
+    precio: number;
+    aerolinea: string;
+    plazas_disponibles: number;
+}
+
+export class usuariosDto {
+    _id: string;
+    origen: string;
+    destino: string;
+    fecha_salida: string;
+    hora_salida: string;
+    hora_llegada: string;
+    precio: number;
+    aerolinea: string;
+    plazas_disponibles: number;
+}
+
+export class reservasDto {
+    _id: string;
+    usuario_id: string;
+    vuelo_id: string;
+    fecha_reserva: string;
+    estado: string;
+    asiento: string;
+}
+
+export class pagosDto {
+    _id: string;
+    reserva_id: string;
+    monto: number;
+    metodo_pago: string;
+    estado_pago: string;
+    fecha_pago: string;
+}
+
+export class opinionesDto {
+    _id: string;
+    usuario_id: string;
+    vuelo_id: string;
+    comentario: string;
+    puntuacion: number;
+    fecha: string;
+}
+
