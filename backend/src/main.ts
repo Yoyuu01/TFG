0c5e49ec0b7229d143e99d216ec6e1c520bf5ab1
yoyuu01 <josuegabriel.barrueta.gutierrez@cieep.com>
2025-05-01 15:33:47 +0200

    cambios main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization'
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`Aplicación ejecutándose en el puerto ${port}`);
 }
+
 bootstrap();
