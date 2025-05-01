8562ddaddd08427c17b0915b3f97335a485bfd26
yoyuu01 <josuegabriel.barrueta.gutierrez@cieep.com>
2025-05-01 15:27:58 +0200

    edition

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,19 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ViajesModule } from './viajes/viajes.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('URL'),
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(
+      process.env.URL as string
+
+    ),
+    ViajesModule,
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
