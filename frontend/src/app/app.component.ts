b0317f3e7b68b24b909044237fc67f319e923ab8
yoyuu01 <josuegabriel.barrueta.gutierrez@cieep.com>
2025-05-06 22:26:18 +0200

    solucion error desconocido consola

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,4 +8,18 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 })
 export class AppComponent {
   constructor() {}
+
+  onMenuOpen() {
+    setTimeout(() => {
+      const firstMenuButton = document.querySelector('ion-menu ion-item, ion-menu button');
+      if (firstMenuButton) (firstMenuButton as HTMLElement).focus();
+    }, 100);
+  }
+
+  onMenuClose() {
+    setTimeout(() => {
+      const mainButton = document.querySelector('.ver-vuelo-btn');
+      if (mainButton) (mainButton as HTMLElement).focus();
+    }, 100);
+  }
 }
